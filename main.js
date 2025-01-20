8c88c90ec36d92075423a3805a6880de913787d1
Hbouazzaoui <bouazzaouiihamouda@gmail.com>
2025-01-20 11:22:22 +0100

    Challenge 1

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ let head = document.getElementById('text');
 head.innerHTML ='hello word';
 
 //Challenge 2: Create and Append an Element
+const ul = document.getElementById('list');
+const newlist = document.createElement('li');
+newlist.textContent ='list2'; 
+ul.appendChild(newlist);
+
+//Challenge 3:
+
