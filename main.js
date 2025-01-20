076de2121f5414a841450583a12b23861e583a42
Hbouazzaoui <bouazzaouiihamouda@gmail.com>
2025-01-20 13:02:22 +0100

    Challenge 5: Form Validation

--- main.js
+++ main.js
@@ -10,8 +10,10 @@ ul.appendChild(newlist);
 
 //Challenge 3:
 const btn = document.getElementById('styleButton');
-btn.style.backgroundColor = 'blue';
-btn.style.color = "white";
+btn.addEventListener('click', ()=> {
+    btn.style.backgroundColor = 'blue';
+    btn.style.color = "white";
+})
 
 //Challenge 4:
 const buttons = document.querySelectorAll('.alertButton');
@@ -21,3 +23,25 @@ buttons.forEach(button => {
     });
 });
 
+//Challenge 5:
+const form = document.getElementById('form');
+const inputField = document.getElementById('inputField');
+const error = document.getElementById('error');
+const valid = document.getElementById('valid');
+
+form.addEventListener('submit' ,function (event){
+
+    if(inputField.value.trim() === ''){
+        event.preventDefault();
+        error.style.display ='block';
+        valid.style.display ='none'
+    }
+    else{
+        event.preventDefault();
+        valid.style.display = 'block';
+        error.style.display ='none';
+    }
+      setTimeout(function() {
+      valid.style.display = 'none'; 
+    }, 2000);
+});
